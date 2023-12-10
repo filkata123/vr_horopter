508bd23a3cdffbb7fd663e70dbd2a04303e5679a
Filip Georgiev <filip.vic.georgiev@gmail.com>
2023-12-10 19:38:05 +0200

    Improved textures by adding a border and making dot size modular

--- Assets/AdjustHoropter.cs
+++ Assets/AdjustHoropter.cs
@@ -10,28 +10,38 @@ public class AdjustHoropter : MonoBehaviour
     [Range(1, 200)]
     public float dotDensity = 100.0f;
 
+    [Range(0.01f, 1)]
+    public float dotSize = 0.2f;
+
     Material mat;
-    // Start is called before the first frame update
     void Start()
     {
         mat = GetComponent<Renderer>().material;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (mat == null) { Debug.Log("No material found on object."); return; }
         mat.SetFloat("_CurveStrength", curveStrength);
         mat.SetFloat("_DotDensity", dotDensity);
+        mat.SetFloat("_DotSize", dotSize);
     }
 
+    // Update curve strength based on slider input
     public void UpdateCurveStrength(float strength)
     {
         curveStrength = strength;
     }
 
+    // Update dot density based on slider input
     public void UpdateDotDensity(float density)
     {
         dotDensity = density;
+    }
+
+    // Update dot size based on slider input
+    public void UpdateDotSize(float size)
+    {
+        dotSize = size;
     }
 }
